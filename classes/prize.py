f85a9bd0f468afd07aea84c65c7612bc53456227
Greta <gretams91@gmail.com>
2024-04-12 16:28:07 +0200

    Create prize.py

---
+++ classes/prize.py
@@ -0,0 +1,7 @@
+
+class Prize:
+    def __init__(self, id, title, description, image):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.image = image
